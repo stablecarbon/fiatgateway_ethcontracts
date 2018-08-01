f4ad216cd283193ba6793816fc258380f0050b60
Nicholas Pai <npai.nyc@gmail.com>
2018-08-01 10:24:04 -0400

    CarbonDollarProxy tested

---
+++ contracts/upgradeability/DelayedUpgradeabilityProxy.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.23;
+
+import "zos-lib/contracts/upgradeability/UpgradeabilityProxy.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract DelayedUpgradeabilityProxy is UpgradeabilityProxy {
+	
+}
