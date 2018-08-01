26a5442f66d1f878c38c3ed27e83364d8112119c
Nicholas Pai <npai.nyc@gmail.com>
2018-08-01 10:28:21 -0400

    deleted unused mocks

--- test/helpers/mocks.js
+++ test/helpers/mocks.js
@@ -1,13 +1,7 @@
 const PermissionSheetMock = artifacts.require("PermissionSheetMock");
 const ValidatorSheetMock = artifacts.require("ValidatorSheetMock");
-const WhitelistedTokenRegulatorMock = artifacts.require("WhitelistedTokenRegulatorMock");
-const CarbonDollarRegulatorMock = artifacts.require("CarbonDollarRegulatorMock");
-const RegulatorFullyLoadedMock = artifacts.require("RegulatorFullyLoadedMock");
 
 module.exports = {
 	PermissionSheetMock,
-	ValidatorSheetMock,
-	WhitelistedTokenRegulatorMock,
-	CarbonDollarRegulatorMock,
-	RegulatorFullyLoadedMock
+	ValidatorSheetMock
 }
