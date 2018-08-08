d11e4cae748ea267db21c91054a4874fdc61cddb
Nicholas Pai <npai.nyc@gmail.com>
2018-08-08 11:25:49 -0400

    deployed CD Token in 2_deploy...

--- migrations/2_deploy_CD_regulator.js
+++ migrations/2_deploy_CD_regulator.js
@@ -1,6 +1,12 @@
 var PermissionSheetMock = artifacts.require("./PermissionSheetMock");
 var ValidatorSheet = artifacts.require("./ValidatorSheet");
 var CarbonDollarRegulator = artifacts.require("./CarbonDollarRegulator");
+var BalanceSheet = artifacts.require("./BalanceSheet");
+var AllowanceSheet = artifacts.require("./AllowanceSheet");
+var FeeSheet = artifacts.require("./FeeSheet");
+var StablecoinWhitelist = artifacts.require("./StablecoinWhitelist");
+var CarbonDollar = artifacts.require("./CarbonDollar");
+
 var RegulatorProxy = artifacts.require("./RegulatorProxy")
 
 module.exports = function(deployer, network, accounts) {
@@ -9,15 +15,11 @@ module.exports = function(deployer, network, accounts) {
   let carbonDollarInstance = null;
   let permissionSheetInstance = null;
   let validatorSheetInstance = null;
-
-  let mint = null;
-  let mint_cusd = null
-  let convert_wt = null
-  let convert_cd = null
-  let burn = null
-  let destroy_blacklisted = null
-  let spend_blacklisted = null
-  let blacklisted = null
+  let carbonDollarTokenInstance = null;
+  let balanceSheetInstance = null;
+  let allowanceSheetInstance = null;
+  let feeSheetInstance = null
+  let whitelistInstance = null
 
   // CARBONDOLLAR
 
@@ -25,30 +27,6 @@ module.exports = function(deployer, network, accounts) {
   deployer.deploy(PermissionSheetMock, {from:regulator}).then(function () {
     return PermissionSheetMock.deployed().then(function (instance) {
       permissionSheetInstance = instance
-      return permissionSheetInstance.MINT_SIG().then(function (sig) {
-        mint = sig
-        return permissionSheetInstance.MINT_CUSD_SIG().then(function (sig) {
-          mint_cusd = sig
-          return permissionSheetInstance.CONVERT_WT_SIG().then(function (sig) {
-            convert_wt = sig
-            return permissionSheetInstance.CONVERT_CARBON_DOLLAR_SIG().then(function (sig) {
-              convert_cd = sig
-              return permissionSheetInstance.BURN_SIG().then(function (sig) {
-                burn = sig
-                return permissionSheetInstance.DESTROY_BLACKLISTED_TOKENS_SIG().then(function (sig) {
-                  destroy_blacklisted = sig
-                  return permissionSheetInstance.APPROVE_BLACKLISTED_ADDRESS_SPENDER_SIG().then(function (sig) {
-                    spend_blacklisted = sig
-                    return permissionSheetInstance.BLACKLISTED_SIG().then(function (sig) {
-                      blacklisted = sig
-                    })
-                  })
-                })
-              })
-            })
-          })
-        })
-      })
     })
   }).then(function () {
   // ValidatorSheet with one validator
@@ -67,6 +45,48 @@ module.exports = function(deployer, network, accounts) {
               // Set regulator as minter, user as whitelisted
               carbonDollarInstance.setMinter(regulator, {from:regulator})
               carbonDollarInstance.setWhitelistedUser(user, {from:regulator})
+            }).then(function () {
+              // Deploy CD Token data stores
+                deployer.deploy(BalanceSheet, {from:regulator}).then(function () {
+                  return BalanceSheet.deployed().then(function (instance) {
+                    balanceSheetInstance = instance
+                  })
+                }).then(function () {
+                  deployer.deploy(AllowanceSheet, {from:regulator}).then(function () {
+                    return AllowanceSheet.deployed().then(function (instance) {
+                      allowanceSheetInstance = instance
+                    })
+                  }).then(function () {
+                    deployer.deploy(FeeSheet, {from:regulator}).then(function () {
+                      return FeeSheet.deployed().then(function (instance) {
+                        feeSheetInstance = instance
+                      })
+                    }).then(function () {
+                      deployer.deploy(StablecoinWhitelist, {from:regulator}).then(function () {
+                        return StablecoinWhitelist.deployed().then(function (instance) {
+                          whitelistInstance = instance
+                        })
+                      }).then(function () {
+                        deployer.deploy(CarbonDollar, CarbonDollarRegulator.address, BalanceSheet.address, AllowanceSheet.address, FeeSheet.address, StablecoinWhitelist.address, {from:regulator}).then(function () {
+                          return CarbonDollar.deployed().then(function (instance) {
+                            carbonDollarTokenInstance = instance
+                            // Transfer ownerships
+                            return balanceSheetInstance.transferOwnership(carbonDollarTokenInstance.address, {from:regulator}).then(function () {
+                              return allowanceSheetInstance.transferOwnership(carbonDollarTokenInstance.address, {from:regulator}).then(function () {
+                                return feeSheetInstance.transferOwnership(carbonDollarTokenInstance.address, {from:regulator}).then(function () {
+                                  return whitelistInstance.transferOwnership(carbonDollarTokenInstance.address, {from:regulator}).then(function () {
+                                    // CD Regulator needs to whitelist CD Token
+                                    carbonDollarInstance.setWhitelistedUser(carbonDollarTokenInstance.address, {from:regulator})
+                                  })
+                                })
+                              })
+                            })
+                          })
+                        })
+                      })
+                    })
+                  })
+                })
             })
           })
         })
