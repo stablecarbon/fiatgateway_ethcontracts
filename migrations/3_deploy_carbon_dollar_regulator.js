e6a2b66dda558259db100a43213d3b8a33ee4ec9
Nicholas Pai <npai.nyc@gmail.com>
2018-08-10 14:10:49 -0400

    deploy CD

--- migrations/3_deploy_carbon_dollar_regulator.js
+++ migrations/3_deploy_carbon_dollar_regulator.js
@@ -1,23 +1,23 @@
-var RegulatorProxyFactory = artifacts.require("./RegulatorProxyFactory");
-var PermissionSheet = artifacts.require("./PermissionSheet")
-var ValidatorSheet = artifacts.require("./ValidatorSheet")
-var Regulator = artifacts.require("./Regulator")
+var ValidatorSheet = artifacts.require("./ValidatorSheet");
+var PermissionSheetMock = artifacts.require("./PermissionSheetMock")
+var CarbonDollarRegulator = artifacts.require("./CarbonDollarRegulator")
+var RegulatorProxy = artifacts.require("./RegulatorProxy")
 
 module.exports = function(deployer, network, accounts) {
-  let regulator = accounts[0];
+  let cdRegulatorOwner = accounts[0];
   let token = accounts[1];
 
   // Testing regular Regulator 
-  deployer.deploy(PermissionSheet, {from:regulator})
-  deployer.deploy(ValidatorSheet, {from:regulator})
-  // deployer.deploy(Regulator, PermissionSheet.address, ValidatorSheet.address, {from:regulator})
-  // deployer.deploy(RegulatorLogicFactory, {from:regulator})
-  // deployer.deploy(RegulatorProxyFactory, {from:regulator})
-
-  // PermissionedToken 
-  // deployer.deploy(BalanceSheet, {from:token})
-  // deployer.deploy(AllowanceSheet, {from:token})
-  // deployer.deploy(PermissionedToken, Regulator.address, BalanceSheet.address, AllowanceSheet.address, {from:token})
-  // deployer.deploy(PermissionedTokenLogicFactory, {from:token})
-  // deployer.deploy(PermissionedTokenProxyFactory, {from:token})
+  deployer.deploy(PermissionSheetMock, { from: cdRegulatorOwner}).then(function() {
+  deployer.deploy(ValidatorSheet, { from: cdRegulatorOwner }).then(function() {
+  deployer.deploy(CarbonDollarRegulator, PermissionSheetMock.address,
+                                             ValidatorSheet.address, 
+                                             { from: cdRegulatorOwner }).then(function () {
+  deployer.deploy(RegulatorProxy, WhitelistedTokenRegulator.address, 
+                                  PermissionSheetMock.address, 
+                                  ValidatorSheet.address, 
+                                  { from: cdRegulatorOwner })
+  })
+  })
+  })
 };
