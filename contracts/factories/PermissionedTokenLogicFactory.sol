bc67689609616ce963a7ec0a4b3da173e5834c44
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2018-08-10 13:27:03 -0400

    Conformed to style standards (linting)

--- contracts/factories/PermissionedTokenLogicFactory.sol
+++ contracts/factories/PermissionedTokenLogicFactory.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 
-import '../tokens/permissionedToken/PermissionedToken.sol';
+import "../tokens/permissionedToken/PermissionedToken.sol";
 
 
 /**
@@ -11,40 +11,38 @@ import '../tokens/permissionedToken/PermissionedToken.sol';
 *
 **/
 contract PermissionedTokenLogicFactory {
+    // Parameters
+    address[] public tokens;
 
-	// Parameters
-	address[] public tokens;
+    // Events
+    event CreatedPermissionedTokenLogic(address newToken, uint index);
 
-	// Events
-	event CreatedPermissionedTokenLogic(address newToken, uint index);
+    // Return number of token logic contracts created so far
+    function getCount() public view returns (uint) {
+        return tokens.length;
+    }
 
-	// Return number of token logic contracts created so far
-	function getCount() public view returns (uint) {
-		return tokens.length;
-	}
+    // Return the i'th created token
+    function getToken(uint i) public view returns(address) {
+        require((i < tokens.length) && (i >= 0), "Invalid index");
+        return tokens[i];
+    }
 
-	// Return the i'th created token
-	function getToken(uint i) public view returns(address) {
-		require((i < tokens.length) && (i >= 0));
-		return tokens[i];
-	}
+    /**
+    *
+    * @dev generate a new token address that users can cast to a PermissionedToken. The
+    * token has no data storage contracts connected to it, so users must call
+    * set[Permission]/[Validator]Storage or use it as the implementation address
+    * for a PermissionedTokenProxy
+    *
+    **/
+    function createToken() public {
+        
+        // Store new data storage contracts for token
 
-	/**
-	*
-	* @dev generate a new token address that users can cast to a PermissionedToken. The
-	* token has no data storage contracts connected to it, so users must call
-	* set[Permission]/[Validator]Storage or use it as the implementation address
-	* for a PermissionedTokenProxy
-	*
-	**/
-	function createToken() public {
-		
-		// Store new data storage contracts for token
-
-		address tokenLogic = address(new PermissionedToken(address(0), address(0), address(0)));
-		tokens.push(tokenLogic);
-		emit CreatedPermissionedTokenLogic(tokenLogic, getCount()-1);
-	}
-
+        address tokenLogic = address(new PermissionedToken(address(0), address(0), address(0)));
+        tokens.push(tokenLogic);
+        emit CreatedPermissionedTokenLogic(tokenLogic, getCount()-1);
+    }
 }
 
