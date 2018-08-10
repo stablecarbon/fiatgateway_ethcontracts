99133d2ece251cdfe24f9f2c5bcd462d6eeffd9f
Nicholas Pai <npai.nyc@gmail.com>
2018-08-10 16:58:08 -0400

    deploy WT

---
+++ migrations/10_deploy_whitelisted_token_proxy.js
@@ -0,0 +1,49 @@
+var AllowanceSheetFactory = artifacts.require("./AllowanceSheetFactory");
+var BalanceSheetFactory = artifacts.require("./BalanceSheetFactory")
+var RegulatorProxyFactory = artifacts.require("./RegulatorProxyFactory");
+var WhitelistedToken = artifacts.require("./CarbonDollar");
+var CarbonDollarProxy = artifacts.require("./CarbonDollarProxy");
+
+module.exports = function (deployer, network, accounts) {
+    let wtTokenOwner = accounts[0];
+    var allowanceSheetInstance
+    var balanceSheetInstance
+    const ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
+
+    RegulatorProxyFactory.deployed().then(function (proxyRegulatorInstance) {
+        proxyRegulatorInstance.getCount().then(function (count) {
+            proxyRegulatorInstance.getRegulatorProxy(count - 1).then(function (wtRegulatorInstance) {
+                AllowanceSheetFactory.deployed().then(function (allowanceFactoryInstance) {
+                    allowanceFactoryInstance.createAllowanceSheet().then(function () {
+                        allowanceFactoryInstance.getCount().then(function (count) {
+                            allowanceFactoryInstance.getAllowanceSheet(count - 1).then(function (newSheet) {
+                                allowanceSheetInstance = newSheet
+                            })
+                        })
+                    })
+                }).then(function () {
+                    BalanceSheetFactory.deployed().then(function (balanceFactoryInstance) {
+                        balanceFactoryInstance.createBalanceSheet().then(function () {
+                            balanceFactoryInstance.getCount().then(function (count) {
+                                balanceFactoryInstance.getBalanceSheet(count - 1).then(function (newSheet) {
+                                    balanceSheetInstance = newSheet
+                                })
+                            })
+                        })
+                    }).then(function () {
+                        CarbonDollarProxy.deployed().then(function(cusd) {
+                            deployer.deploy(WhitelistedToken, 
+                                            wtRegulatorInstance, 
+                                            balanceSheetInstance, 
+                                            allowanceSheetInstance,
+                                            cusd.address, 
+                                            {from:wtTokenOwner, gas:100000000})
+                        })
+                    })
+                })
+            })
+        })
+    })
+
+    
+};
--- migrations/9_deploy_whitelisted_token.js
+++ migrations/9_deploy_whitelisted_token.js
@@ -1,47 +1,9 @@
-var AllowanceSheetFactory = artifacts.require("./AllowanceSheetFactory");
-var BalanceSheetFactory = artifacts.require("./BalanceSheetFactory");
-var RegulatorProxyFactory = artifacts.require("./RegulatorProxyFactory")
 var WhitelistedToken = artifacts.require("./WhitelistedToken");
-var CarbonDollarProxy = artifacts.require("./CarbonDollarProxy");
 
 module.exports = function (deployer, network, accounts) {
     let wtTokenOwner = accounts[0];
-    var allowanceSheetInstance
-    var balanceSheetInstance
+    const ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
+
+    // deployer.deploy(WhitelistedToken, ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS, {from:wtTokenOwner})
     
-    // RegulatorProxyFactory.deployed().then(function (proxyRegulatorInstance) {
-    //     proxyRegulatorInstance.getCount().then(function (count) {
-    //         proxyRegulatorInstance.getRegulatorProxy(count - 1).then(function (wtRegulatorInstance) {
-    //             CarbonDollarProxy.deployed().then(function (cusdInstance) {
-    //                 AllowanceSheetFactory.deployed().then(function (allowanceFactoryInstance) {
-    //                     allowanceFactoryInstance.createAllowanceSheet().then(function () {
-    //                         allowanceFactoryInstance.getCount().then(function (count) {
-    //                             allowanceFactoryInstance.getAllowanceSheet(count - 1).then(function (newSheet) {
-    //                                 allowanceSheetInstance = newSheet
-    //                                 console.log('WT: new vsheet: ' + allowanceSheetInstance)
-    //                             })
-    //                         })
-    //                     })
-    //                 }).then(function () {
-    //                     BalanceSheetFactory.deployed().then(function (balanceFactoryInstance) {
-    //                         balanceFactoryInstance.createBalanceSheet().then(function () {
-    //                             balanceFactoryInstance.getCount().then(function (count) {
-    //                                 balanceFactoryInstance.getBalanceSheet(count - 1).then(function (newSheet) {
-    //                                     balanceSheetInstance = newSheet
-    //                                     console.log('WT: new psheet: ' + balanceSheetInstance)
-    //                                 })
-    //                             })
-    //                         })
-    //                     }).then(function () {
-    //                         deployer.deploy(WhitelistedToken,
-    //                             wtRegulatorInstance.address,
-    //                             balanceSheetInstance.address,
-    //                             allowanceSheetInstance.address,
-    //                             cusdInstance.address, { from: wtTokenOwner })
-    //                         });
-    //                 });
-    //             });
-    //         });
-    //     });
-    // })
-}
+};
