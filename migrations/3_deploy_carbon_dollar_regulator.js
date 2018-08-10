c4029738ed1f77658299261afb54abd215831af5
Nicholas Pai <npai.nyc@gmail.com>
2018-08-10 14:31:13 -0400

    test

--- migrations/3_deploy_carbon_dollar_regulator.js
+++ migrations/3_deploy_carbon_dollar_regulator.js
@@ -8,16 +8,8 @@ module.exports = function(deployer, network, accounts) {
   let token = accounts[1];
 
   // Testing regular Regulator 
-  deployer.deploy(PermissionSheetMock, { from: cdRegulatorOwner}).then(function() {
-  deployer.deploy(ValidatorSheet, { from: cdRegulatorOwner }).then(function() {
-  deployer.deploy(CarbonDollarRegulator, PermissionSheetMock.address,
-                                             ValidatorSheet.address, 
-                                             { from: cdRegulatorOwner }).then(function () {
-  deployer.deploy(RegulatorProxy, WhitelistedTokenRegulator.address, 
-                                  PermissionSheetMock.address, 
-                                  ValidatorSheet.address, 
-                                  { from: cdRegulatorOwner })
+  PermissionSheetMock.deployed().then(function(permissionSheet) {
+    console.log(permissionSheet)
   })
-  })
-  })
+
 };
